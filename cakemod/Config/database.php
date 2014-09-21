8fae869af0734a9b34a5bb41a8a008a5f68a3e29
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2014-09-21 15:50:38 -0400

    CakeMod Initialized

---
+++ cakemod/Config/database.php
@@ -0,0 +1,22 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $cakemod_admin = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'port' => 3306,
+		'login' => 'root',
+		'password' => 'aco1234',
+		'database' => 'acoserver_cakemod',
+	);
+	public $default = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => false,
+		'host' => 'localhost',
+                 'port' => 3306,
+		'login' => 'root',
+		'password' => 'aco11234',
+		'database' => 'acoserver_cakemod',
+	);
+}
