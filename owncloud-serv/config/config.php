0383992a517d54327f6ac036ab1d4ad06b7cb06a
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2014-09-21 17:03:33 -0400

    Owncloud

---
+++ owncloud-serv/config/config.php
@@ -0,0 +1,19 @@
+<?php
+$CONFIG = array (
+  'instanceid' => 'ocd83a5244b7',
+  'passwordsalt' => 'f6b8a449e0c9e5e1da7cd116ef9c7c',
+  'trusted_domains' => 
+  array (
+    0 => 'localhost',
+  ),
+  'datadirectory' => 'K:\\ArcherVMPeridot\\htdocs\\owncloud-serv\\data',
+  'dbtype' => 'mysql',
+  'version' => '7.0.2.1',
+  'dbname' => 'acoserver_acoserver',
+  'dbhost' => 'localhost',
+  'dbtableprefix' => 'oc_',
+  'dbuser' => 'oc_archerroot',
+  'dbpassword' => '4bb415c7cd23227dad5bd39d57d070',
+  'installed' => true,
+  'ldapIgnoreNamingRules' => false,
+);
