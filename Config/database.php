b7be05b9e33b5316d4095a978d1e735df9518526
unknown <Student@PS458S-5KR6MVIOIN60.students.dc_schools.k12.dc.us>
2014-09-21 22:49:43 -0400

    New

---
+++ Config/database.php
@@ -0,0 +1,14 @@
+<?php
+class DATABASE_CONFIG {
+
+	public $ArcherSys_CakeMod = array(
+		'datasource' => 'Database/Mysql',
+		'persistent' => true,
+		'host' => 'localhost',
+		'port' => 3306,
+		'login' => 'root',
+		'password' => 'aco1234',
+		'database' => 'acoserver_cakemod',
+		'prefix' => 'cakemod',
+	);
+}
