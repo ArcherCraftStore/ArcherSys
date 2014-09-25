36780dca5bf36ef2a1b77cf169719eff78526538
unknown <weldon23.henson23@gmail.com>
2014-09-25 10:22:00 -0400

    New FIles

--- owncloud-serv/config/config.php
+++ owncloud-serv/config/config.php
@@ -1,19 +1,18 @@
 <?php
 $CONFIG = array (
-  'instanceid' => 'ocd83a5244b7',
-  'passwordsalt' => 'f6b8a449e0c9e5e1da7cd116ef9c7c',
+  'instanceid' => 'ocb2f9d4fd41',
+  'passwordsalt' => '59023877bf92c375898a24ab84bd4a',
   'trusted_domains' => 
   array (
     0 => 'localhost',
   ),
-  'datadirectory' => 'K:\\ArcherVMPeridot\\htdocs\\owncloud-serv\\data',
+  'datadirectory' => 'E:\\xampp\\htdocs\\owncloud-serv\\data',
   'dbtype' => 'mysql',
   'version' => '7.0.2.1',
   'dbname' => 'acoserver_acoserver',
   'dbhost' => 'localhost',
   'dbtableprefix' => 'oc_',
-  'dbuser' => 'oc_archerroot',
-  'dbpassword' => '4bb415c7cd23227dad5bd39d57d070',
+  'dbuser' => 'oc_archerroot2',
+  'dbpassword' => '5b02224c8676c48904608228000406',
   'installed' => true,
-  'ldapIgnoreNamingRules' => false,
 );
